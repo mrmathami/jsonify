d50f636f755c305bd9d92992ac78938366c4f152
thanhminhmr <thanhminh.mr@gmail.com>
2022-02-14 02:51:46 +0700

    Remove JsonPrimitive::of for primitives as they are redundant

--- src/main/java/io/github/mrmathami/jsonify/JsonPrimitive.java
+++ src/main/java/io/github/mrmathami/jsonify/JsonPrimitive.java
@@ -33,38 +33,6 @@ public final class JsonPrimitive implements JsonElement {
 		this.value = value;
 	}
 
-	public static @NotNull JsonPrimitive of(boolean value) {
-		return value ? TRUE : FALSE;
-	}
-
-	public static @NotNull JsonPrimitive of(byte value) {
-		return of(Byte.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(short value) {
-		return of(Short.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(int value) {
-		return of(Integer.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(long value) {
-		return of(Long.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(float value) {
-		return of(Float.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(double value) {
-		return of(Double.valueOf(value));
-	}
-
-	public static @NotNull JsonPrimitive of(char value) {
-		return of(String.valueOf(value));
-	}
-
 	public static @NotNull JsonPrimitive of(@NotNull Boolean value) {
 		return new JsonPrimitive(value);
 	}
